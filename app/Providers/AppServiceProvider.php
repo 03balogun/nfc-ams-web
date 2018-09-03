559e69bf0b77793d11ded646b564ade97d3c966b
Balogun Wahab <bwahab@vasconsolutions.com>
2018-09-03 16:22:50 +0100

    fixed https error on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV') !== 'local')
+        {
+            \URL::forceScheme('https');
+        }
+        Schema::defaultStringLength(191);
     }
 
     /**
